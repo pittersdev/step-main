b1f3d4d45053ba882f719b377b911403fbb997a0
pittersDev <pittersluis23@gmail.com>
2021-02-26 16:25:23 -0400

    ultimos cambios finaless

--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -1,60 +1,59 @@
 var span = document.getElementsByClassName('flecha');
 var div = document.getElementsByClassName('ccard');
-var a= 0;
-span[1].onclick=()=>{
+var a = 0;
+span[1].onclick = () => {
     a++;
-    for(var i of div)
-    {
-        if(a==0){i.style.transform='translateX(0px)';}
-        if(a==1){i.style.transform='translateX(-300px)';}
-        if(a==2){i.style.transform='translateX(-600px)'}
-        if(a==3){i.style.transform='translateX(-900px)';}
-        if(a==4){i.style.transform='translateX(-1200px)';}
-        if(a>4){i.style.transform='translateX(0px)';a=0}
+    for (var i of div) {
+        if (a == 0) { i.style.transform = 'translateX(0px)'; }
+        if (a == 1) { i.style.transform = 'translateX(-300px)'; }
+        if (a == 2) { i.style.transform = 'translateX(-600px)' }
+        if (a == 3) { i.style.transform = 'translateX(-900px)'; }
+        if (a == 4) { i.style.transform = 'translateX(-1200px)'; }
+        if (a > 4) {
+            i.style.transform = 'translateX(0px)';
+            a = 0
+        }
     }
-    
+
 }
-span[0].onclick=()=>{
+span[0].onclick = () => {
     a--;
-    for(var i of div)
-    {
-        if(a==0){i.style.transform='translateX(0px)';}
-        if(a==1){i.style.transform='translateX(-300px)';}
-        if(a==2){i.style.transform='translateX(-600px)'}
-        if(a==3){i.style.transform='translateX(-900px)';}
-        if(a==4){i.style.transform='translateX(-1200px)';}
-        if(a<0){a+1}
-       
+    for (var i of div) {
+        if (a == 0) { i.style.transform = 'translateX(0px)'; }
+        if (a == 1) { i.style.transform = 'translateX(-300px)'; }
+        if (a == 2) { i.style.transform = 'translateX(-600px)' }
+        if (a == 3) { i.style.transform = 'translateX(-900px)'; }
+        if (a == 4) { i.style.transform = 'translateX(-1200px)'; }
+        if (a < 0) { a + 1 }
+
     }
-    
+
 }
-span[3].onclick=()=>{
+span[3].onclick = () => {
     a++;
-    for(var i of div)
-    {
-        if(a==0){i.style.transform='translateX(0px)';}
-        if(a==1){i.style.transform='translateX(-300px)';}
-        if(a==2){i.style.transform='translateX(-600px)'}
-        if(a==3){i.style.transform='translateX(-900px)';}
-        if(a==4){i.style.transform='translateX(-1200px)';}
-        if(a>4){i.style.transform='translateX(0px)';a=0}
+    for (var i of div) {
+        if (a == 0) { i.style.transform = 'translateX(0px)'; }
+        if (a == 1) { i.style.transform = 'translateX(-300px)'; }
+        if (a == 2) { i.style.transform = 'translateX(-600px)' }
+        if (a == 3) { i.style.transform = 'translateX(-900px)'; }
+        if (a == 4) { i.style.transform = 'translateX(-1200px)'; }
+        if (a > 4) {
+            i.style.transform = 'translateX(0px)';
+            a = 0
+        }
     }
-    
+
 }
-span[2].onclick=()=>{
+span[2].onclick = () => {
     a--;
-    for(var i of div)
-    {
-        if(a==0){i.style.transform='translateX(0px)';}
-        if(a==1){i.style.transform='translateX(-300px)';}
-        if(a==2){i.style.transform='translateX(-600px)'}
-        if(a==3){i.style.transform='translateX(-900px)';}
-        if(a==4){i.style.transform='translateX(-1200px)';}
-        if(a<0){a+1}
-       
+    for (var i of div) {
+        if (a == 0) { i.style.transform = 'translateX(0px)'; }
+        if (a == 1) { i.style.transform = 'translateX(-300px)'; }
+        if (a == 2) { i.style.transform = 'translateX(-600px)' }
+        if (a == 3) { i.style.transform = 'translateX(-900px)'; }
+        if (a == 4) { i.style.transform = 'translateX(-1200px)'; }
+        if (a < 0) { a + 1 }
+
     }
-    
+
 }
-
-
-
